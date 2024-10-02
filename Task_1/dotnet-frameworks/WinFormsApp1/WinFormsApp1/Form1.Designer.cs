99a75875d4af0c823eae579aa4ba1b44dc54114b
Mohammad Tasleem <Mohammad_Tasleem@epam.com>
2024-10-02 14:25:24 +0530

    added WInForms

---
+++ Task_1/dotnet-frameworks/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            output = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(282, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(274, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(119, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Please enter username";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(303, 118);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // output
+            // 
+            output.AutoSize = true;
+            output.Location = new Point(264, 189);
+            output.Name = "output";
+            output.Size = new Size(0, 20);
+            output.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(output);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "My App";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Label output;
+    }
+}
