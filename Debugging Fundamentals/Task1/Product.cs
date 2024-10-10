bfd609f2620a7141ba82ecc9b22d6eab7da355d4
Mohammad Tasleem <Mohammad_Tasleem@epam.com>
2024-10-10 10:04:02 +0530

    added Debugging fundamental modules

---
+++ Debugging Fundamentals/Task1/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != this.GetType())
+                return false;
+            Product other = (Product)obj;
+            return this.Name == other.Name && this.Price == other.Price;
+        }
+        // Override GetHashCode to ensure consistency with Equals
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
